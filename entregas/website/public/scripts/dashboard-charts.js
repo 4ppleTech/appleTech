f1d8f410662413a0729376644a9e33f34c45e6c9
arthurr-rosante <arrosante@gmail.com>
2026-04-08 18:13:42 -0300

    feat: otimizei a função que atualiza o gráfico de linhas

--- entregas/website/public/scripts/dashboard-charts.js
+++ entregas/website/public/scripts/dashboard-charts.js
@@ -1,8 +1,8 @@
-const line_chart_div = document.getElementById("line-chart");
-const column_chart_div = document.getElementById("column-chart");
+const lineChartDiv = document.getElementById("line-chart");
+const columnChartDiv = document.getElementById("column-chart");
 
 // === GRÁFICO DE LINHAS === //
-const line_data = {
+const lineChartData = {      // dados mockados para visualização no gráfico
     '24h': {
         labels: ['00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00', '23:59'],
         data: [0, 2.6, 1.3, 1.3, 4.3, 2.5, 2.5, 0.7, 2.6, 1.2]
@@ -16,66 +16,69 @@ const line_data = {
         data: [1.5, 2.8, 3.9, 2.2]
     }
 }
-let line_chart = new Chart(line_chart_div, {
-  type: 'line',
-  data: {
-    labels: line_data['24h'].labels,
-    datasets: [{
-      data: line_data['24h'].data,
-      borderColor: '#7a9b55',
-      backgroundColor: 'rgba(122, 155, 85, 0.2)',
-      fill: true,
-      tension: 0.4,
-      pointRadius: 0,
-      borderWidth: 2
-    }]
-  },
-  options: {
-    responsive: true,
-    plugins: {
-        legend: { 
-            display: false 
-        },
+
+// criação/configuração do gráfico
+const lineChart = new Chart(lineChartDiv, {
+    type: "line",
+    data: {
+        labels: lineChartData['24h'].labels,                // legendas do eixo X
+        datasets: [{
+            data: lineChartData['24h'].data,                // dados do gráfico
+            borderColor: '#7a9b55',                       // cor do contorno
+            backgroundColor: 'rgba(122, 155, 85, 0.2)',   // cor da linha
+            fill: true,                                     // colore o espaço interno
+            tension: 0.4,                                   // forneçe o aspecto "curvado" da linha
+            pointRadius: 0,                                 // elimina os pontinhos da linha
+            borderWidth: 2                                  // determina grossura da linha
+        }]
     },
-    scales: {
-      y: {
-        min: 0,
-        max: 5,
-        ticks: {
-            stepSize: 1.0,
+    options: {
+        responsive: true,                                   // deixa o tamanho do gráfico adaptável
+        plugins: {
+            legend: { display: false },                     // remove a legenda para os dados
         },
-        grid: {
-          drawBorder: false,
+        scales: {
+            y: {
+                min: 0,                                     // valor mínimo do eixo Y = 0
+                max: 5,                                     // valor máximo do eixo Y = 5
+                ticks: { stepSize: 1.0 },                   // delimita "salto" de sempre 1.0 entre as medições do eixo Y
+                grid: { drawBorder: false }                 // não desenha as bordas do eixo Y
+            },
+            x: {
+            grid: { display: false }                        // não desenha as linhas do eixo X
+            }
         }
-      },
-      x: {
-        grid: { display: false }
-      }
     }
-  }
 });
 
-function atualizarData(periodo) {
-  line_chart.data.labels = line_data[periodo].labels;
-  line_chart.data.datasets[0].data = line_data[periodo].data;
-  line_chart.update();
+function atualizarPeriodo(periodo) {                                    // recebe '24h', '7d' ou '30d'
+    lineChart.data.labels = lineChartData[periodo].labels;              // configura as labels do período correspondente
+    lineChart.data.datasets[0].data = lineChartData[periodo].data;      // configura os dados do período correspondente
+    lineChart.update();                                                 // ! atualiza o gráfico !
 
-  document.querySelectorAll('.btn-group button').forEach(btn => btn.classList.remove('active'));
-  event.target.classList.add('active');
+    // confere a classe "active" ao botão ativo no momento e remove-a dos demais
+    let btnGtoup = document.querySelectorAll(".btn-group button");      
+    for (let i = 0; i < btnGtoup.length; i++) {
+        btnGtoup[i].classList.remove("active");
+    }
+    let activeBtn = document.querySelector(`.btn-group button[data-periodo="${periodo}"]`);
+    activeBtn.classList.add("active");
+
 }
 
-// === GRÁFICO DE BARRAS === //
-const column_data = {
+
+// // === GRÁFICO DE BARRAS === //
+const columnData = {
     labels: ['C-01', 'C-02', 'C-03', 'C-04', 'C-05', 'Ideal'],
     data: [3.1, 2.0, 1.5, 4.2, 2.2]
 };
 
-let column_chart = new Chart(column_chart_div, {
+let columnChart = new Chart(columnChartDiv, {
     type: 'bar',
     data: {
-        labels: column_data.labels,
+        labels: columnData.labels,
         datasets: [{
-        data: column_data.data,
+        data: columnData.data,
         borderColor: '#7a9b55',
         backgroundColor: 'rgba(122, 155, 85, 0.2)',
         fill: true,
@@ -86,9 +89,7 @@ let column_chart = new Chart(column_chart_div, {
     },
     options: {
         plugins: {
-            legend: { 
-                display: false 
-            },
+            legend: { display: false },
         },
         scales: {
             x: {
