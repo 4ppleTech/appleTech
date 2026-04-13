49f0277b65f728a7c99140faace2b9c2cfe36de9
arthurr-rosante <arrosante@gmail.com>
2026-04-12 23:13:09 -0300

    refactor: adaptei o controller de usuário para as regras de negócio do projeto

--- entregas/website/src/controllers/usuarioController.js
+++ entregas/website/src/controllers/usuarioController.js
@@ -2,8 +2,8 @@ var usuarioModel = require("../models/usuarioModel");
 var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
+    var email = req.body.email;
+    var senha = req.body.senha;
 
     if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
@@ -52,39 +52,29 @@ function autenticar(req, res) {
 }
 
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    var username = req.body.username;
+    var email = req.body.email;
+    var senha = req.body.senha;
+    var idEmpresa = req.body.idEmpresa;
 
     // Faça as validações dos valores
-    if (nome == undefined) {
+    if (username == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
+    } else if (idEmpresa == undefined) {
+        res.status(400).send("Seu código de empresa está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
+        usuarioModel.cadastrar(username, email, senha, idEmpresa)
+            .then((resultado) => res.json(resultado))
+            .catch((err) => {
+                console.log("[usuarioController] ERRO: ", err.sqlMessage);
+                res.status(500).json(err.sqlMessage);
+            })
     }
 }
 
