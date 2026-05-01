84c2b03bb47dde8d527627189f761a70343cf70c
arthurr-rosante <arrosante@gmail.com>
2026-04-30 21:08:26 -0300

    feat: validei mudanças nos gráficos da dashboard

--- entregas/website/public/scripts/dashboard-charts.js
+++ entregas/website/public/scripts/dashboard-charts.js
@@ -5,15 +5,33 @@ const columnChartDiv = document.getElementById("column-chart");
 const lineChartData = {
     '24h': {
         labels: ['00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00', '23:59'],
-        data: [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42] 
+        data: [
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-01
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-02
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-03
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-04
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-05
+        ] 
     },
     '7d': {
         labels: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'],
-        data: [1.2, 1.1, 1.3, 1.4, 1.2, 1.3, 1.4]
+        data: [
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-01
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-02
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-03
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-04
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-05
+        ] 
     },
     '30d': {
         labels: ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4'],
-        data: [1.2, 1.3, 1.2, 1.4]
+        data: [
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-01
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-02
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-03
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-04
+            [1.1, 1.1, 1.0, 1.1, 1.2, 1.3, 1.5, 1.4, 1.42],                     // dados da C-05
+        ] 
     }
 };
 
@@ -22,15 +40,53 @@ const lineChart = new Chart(lineChartDiv, {
     type: "line",
     data: {
         labels: lineChartData['24h'].labels,                // legendas do eixo X
-        datasets: [{
-            data: lineChartData['24h'].data,                // dados do gráfico
+        datasets: [
+        {
+            data: lineChartData['24h'].data[0],                // dados do gráfico
             borderColor: '#7a9b55',                       // cor do contorno
             backgroundColor: 'rgba(122, 155, 85, 0.2)',   // cor da linha
-            fill: true,                                     // colore o espaço interno
+            fill: false,                                     // colore o espaço interno
             tension: 0.4,                                   // forneçe o aspecto "curvado" da linha
-            pointRadius: 0,                                 // elimina os pontinhos da linha
+            pointRadius: 2,                                 // elimina os pontinhos da linha
             borderWidth: 2                                  // determina grossura da linha
-        }]
+        },
+        {
+            data: lineChartData['24h'].data[1],                // dados do gráfico
+            borderColor: '#7a9b55',                       // cor do contorno
+            backgroundColor: 'rgba(122, 155, 85, 0.2)',   // cor da linha
+            fill: false,                                     // colore o espaço interno
+            tension: 0.4,                                   // forneçe o aspecto "curvado" da linha
+            pointRadius: 2,                                 // elimina os pontinhos da linha
+            borderWidth: 2                                  // determina grossura da linha
+        },
+        {
+            data: lineChartData['24h'].data[2],                // dados do gráfico
+            borderColor: '#7a9b55',                       // cor do contorno
+            backgroundColor: 'rgba(122, 155, 85, 0.2)',   // cor da linha
+            fill: false,                                     // colore o espaço interno
+            tension: 0.4,                                   // forneçe o aspecto "curvado" da linha
+            pointRadius: 2,                                 // elimina os pontinhos da linha
+            borderWidth: 2                                  // determina grossura da linha
+        },
+        {
+            data: lineChartData['24h'].data[3],                // dados do gráfico
+            borderColor: '#7a9b55',                       // cor do contorno
+            backgroundColor: 'rgba(122, 155, 85, 0.2)',   // cor da linha
+            fill: false,                                     // colore o espaço interno
+            tension: 0.4,                                   // forneçe o aspecto "curvado" da linha
+            pointRadius: 2,                                 // elimina os pontinhos da linha
+            borderWidth: 2                                  // determina grossura da linha
+        },
+        {
+            data: lineChartData['24h'].data[4],                // dados do gráfico
+            borderColor: '#7a9b55',                       // cor do contorno
+            backgroundColor: 'rgba(122, 155, 85, 0.2)',   // cor da linha
+            fill: false,                                     // colore o espaço interno
+            tension: 0.4,                                   // forneçe o aspecto "curvado" da linha
+            pointRadius: 2,                                 // elimina os pontinhos da linha
+            borderWidth: 2                                  // determina grossura da linha
+        },
+    ]
     },
     options: {
         responsive: true,                                   // deixa o tamanho do gráfico adaptável
