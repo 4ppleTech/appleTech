bc2405a4ae388746a5d04fc983c5b45cee9b263d
marceloo27 <marcelo.carvalho@sptech.school>
2026-06-04 09:05:57 -0300

    Feat: adicionando a coluna preco_kg na tabela empresa

--- entregas/banco_de_dados/appletech-bd.sql
+++ entregas/banco_de_dados/appletech-bd.sql
@@ -30,6 +30,7 @@ create table empresa(
 	razao_social 	varchar(150) not null unique,
 	nome_fantasia 	varchar(150) not null,
 	cnpj varchar(14) not null unique,
+	preco_kg		DECIMAL(10, 2),
 	
     
 	data_criacao 	datetime default current_timestamp,
