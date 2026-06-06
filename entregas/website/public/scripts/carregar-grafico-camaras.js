0b07c9fab4a0aa8927ceebea52a312767793109e
arthurr-rosante <arrosante@gmail.com>
2026-06-06 10:46:20 -0300

    refactor: melhorias visuais e lógicas para as funções que carregam e atualizam dados dos gráficos em chambers.html

--- entregas/website/public/scripts/carregar-grafico-camaras.js
+++ entregas/website/public/scripts/carregar-grafico-camaras.js
@@ -1,8 +1,4 @@
-
-const instanciasGraficos = {};
-
-const coresMap = 
-{
+const coresMap = {
  0: '#B83232',
  1: '#7A9B55',
  2: '#d18b00',
@@ -10,33 +6,40 @@ const coresMap =
  4: '#1A568F'
 }
 
-export function carregarGraficoCamara(chamberId, interval) {
+// HashMap que guarda referência dos gráficos já criados
+const instanciasGraficos = {};
+
+// Aviso: Onde se ver .slice(-10), significa: últimos 10 valores da lista
+export function carregarGraficoCamara(chamberId, interval) { 
     const ctx = document.getElementById(`chart-${chamberId}`);
     if(!ctx) return;
-
-    if(instanciasGraficos[chamberId]) {
-        instanciasGraficos[chamberId].destroy();
-    }
 
     const idCamaraNumerico = parseInt(chamberId.replace("c-", ""));
     fetch(`/medidas/buscar-camara-individual/${idCamaraNumerico}/${interval}`)
         .then((res) => {
-            if (!res.ok) {
-                throw new Error(`Não foi possível carregar gráficos da Câmara: ${chamberId}`)
-            }
+            if(!res.ok) throw new Error(`Não foi possível carregar gráfico da Câmara: ${chamberId}`);
             return res.json();
         })
-        .then((dados) => {
-            const chartLabels = dados.map((d) => d.data_formatada);
+        .then((valores) => {
+            const leiturasMap = {};
+            const chartLabels = [];
 
-            const listaIdSensores = [];
-            dados.forEach((d) => !listaIdSensores.includes(d.id_sensor) && listaIdSensores.push(d.id_sensor));
+            valores.forEach((valor) => {
+                let numeroSensor = valor.numero_sensor;
+                if(leiturasMap[numeroSensor]) {
+                    leiturasMap[numeroSensor].push(valor.etileno);
+                } else {
+                    leiturasMap[numeroSensor] = [valor.etileno];
+                }
 
-            const chartDatasets = listaIdSensores.map((sensorId, index) => {
-                const leiturasDoSensor = dados.filter((d) => d.id_sensor === sensorId);
+                let dataLeitura = new Date(valor.data_hora).toLocaleTimeString();
+                if(!chartLabels.includes(dataLeitura)) chartLabels.push(dataLeitura);
+            });
+
+            const chartDatasets = Object.keys(leiturasMap).map((numeroSensor, index) => {
                 return {
-                    label: leiturasDoSensor[0].numero_sensor,
-                    data: leiturasDoSensor.map((leitura) => ({x: leitura.data_formatada, y: leitura.etileno})),
+                    label: numeroSensor,
+                    data: leiturasMap[numeroSensor].slice(-10),
                     borderColor: coresMap[index],
                     backgroundColor: coresMap[index] + '30',
                     fill: true,
@@ -44,39 +47,55 @@ export function carregarGraficoCamara(chamberId, interval) {
                     pointRadius: 4,
                     borderWidth: 2,
                 };
-            })
+            });
 
-            instanciasGraficos[chamberId] = new Chart(ctx, {
-                type: "line",
-                data: {
-                    labels: chartLabels,
-                    datasets: chartDatasets,
+            // Dados do Gráfico
+            let data = {
+                labels: chartLabels.slice(-10),
+                datasets: chartDatasets
+            };
+            
+            // Configurações do Gráfico
+            let options = {
+                responsive: true,
+                scales: {
+                    x: {
+                        beginAtZero: true,
+                    },
+                    y: {
+                        beginAtZero: true,
+                        suggestedMin: 0,
+                        suggestedMax: 2.5
+                    }
                 },
-                options: {
-                    responsive: true,
-                    plugins: {
-                        annotation: {
-                            annotations: {
-                                limiteCritico: {
-                                    type: "line",
-                                    yMin: 1.5,
-                                    yMax: 1.5,
-                                    borderColor: "#B83232",
-                                    borderWidth: 3,
-                                    label: {
-                                        display: true,
-                                        content: "Limite Crítico (1.5 ppm)",
-                                        position: "end",
-                                        backgroundColor: "rgba(184, 50, 50, 0.8)",
-                                        color: "#fff",
-                                        font: { size: 12, weight: "bold" },
-                                    },
+                plugins: {
+                    annotation: {
+                        annotations: {
+                            limiteCritico: {
+                                type: "line",
+                                yMin: 1.5,
+                                yMax: 1.5,
+                                borderColor: "#B83232",
+                                borderWidth: 3,
+                                label: {
+                                    display: true,
+                                    content: "Limite Crítico (1.5 ppm)",
+                                    position: "end",
+                                    backgroundColor: "rgba(184, 50, 50, 0.8)",
+                                    color: "#fff",
+                                    font: { size: 12, weight: "bold" },
                                 },
                             },
                         },
                     },
-                    scales: { y: { min: 0, max: 3 } },
                 },
+            };
+
+            // Cria Gráfico e guarda referência no HashMap
+            instanciasGraficos[chamberId] = new Chart(ctx, {
+                type: "line",
+                data,
+                options
             });
         })
         .catch((error) => {
@@ -86,56 +105,48 @@ export function carregarGraficoCamara(chamberId, interval) {
 
 export function atualizarGraficoCamara(chamberId) {
     const chart = instanciasGraficos[chamberId];
-    if (!chart) {
-        console.warn(`[atualizarGraficoCamara] Gráfico da câmara ${chamberId} não foi inicializado ainda.`);
+    if(!chart) {
+        carregarGraficoCamara(chamberId, 1);
         return;
-    }
+    };
 
-    let interval = '1';
+    let interval = 1;
     const activeBtn = document.querySelector(`#${chamberId} .btn-group button.active`);
-    if(activeBtn) {
-        interval = activeBtn.dataset.periodo;
-    }
+    if(activeBtn) interval = activeBtn.dataset.periodo;
 
     const idCamaraNumerico = parseInt(chamberId.replace("c-", ""));
     fetch(`/medidas/buscar-camara-individual/${idCamaraNumerico}/${interval}`)
         .then((res) => {
-            if (!res.ok) throw new Error(`Erro ao atualizar dados da Câmara: ${chamberId}`);
+            if(!res.ok) throw new Error(`Erro ao atualizar dados da Câmara: ${chamberId}`);
             return res.json();
         })
-        .then((dados) => {
-            const chartLabels = dados.map((d) => d.data_formatada);
-            chart.data.labels = chartLabels;
+        .then((valores) => {
+            const chartLabels = [];
+            const leiturasMap = {};
 
-            const listaIdSensores = [];
-            dados.forEach((d) => !listaIdSensores.includes(d.id_sensor) && listaIdSensores.push(d.id_sensor));
-
-            listaIdSensores.forEach((sensorId) => {
-                const leiturasDoSensor = dados.filter((d) => d.id_sensor === sensorId);
-
-                let dataset = chart.data.datasets.find((ds) => ds.label === leiturasDoSensor[0].numero_sensor);
-                const novosDados = leiturasDoSensor.map((leitura) => ({x: leitura.data_formatada, y: leitura.etileno}));
-
-                // se dataset já existe
-                if(dataset) {
-                    dataset.data = novosDados
+            valores.forEach((valor) => {
+                let numeroSensor = valor.numero_sensor;
+                if(leiturasMap[numeroSensor]) {
+                    leiturasMap[numeroSensor].push(valor.etileno);
                 } else {
-                // se outro dataset apareceu
-                    chart.data.datasets.push({
-                        label: leiturasDoSensor[0].numero_sensor,
-                        data: novosDados,
-                        borderColor: "#7A9B55",
-                        backgroundColor: "rgba(122, 155, 85, 0.2)",
-                        fill: true,
-                        tension: 0.4,
-                        pointRadius: 4,
-                        borderWidth: 2,
-                    });
-                }  
-            })
+                    leiturasMap[numeroSensor] = [valor.etileno];
+                }
+
+                let dataLeitura = new Date(valor.data_hora).toLocaleTimeString();
+                if(!chartLabels.includes(dataLeitura)) chartLabels.push(dataLeitura);
+            });
+
+            // Atualiza Labels
+            chart.data.labels = chartLabels.slice(-10);
+
+            // Atualiza Datasets
+            Object.keys(leiturasMap).forEach((numeroSensor, index) => {
+                let datasetCorrespondente = chart.data.datasets.find((dataset) => dataset.label === numeroSensor);
+                const novosDados = leiturasMap[numeroSensor].slice(-10);
+                datasetCorrespondente.data = novosDados;
+            });
 
             chart.update();
-            console.log(`[atualizarGraficoCamara] Atualizei Gráfico [chart-${chamberId}]`)
         })
         .catch((error) => {
             console.error(`Erro no update do gráfico ${chamberId}:`, error);
